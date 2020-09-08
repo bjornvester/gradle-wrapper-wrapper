804dfede5ce42b9154917d492e3ed28c460edfc6
Bjørn Vester <bmv@netcompany.com>
2020-09-08 11:07:19 +0200

    Cache more resources in Github Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,11 @@ dependencies {
 
 graal {
     // See https://github.com/palantir/gradle-graal for options
-    graalVersion("20.2.0")
+    graalVersion("20.2.0") // When updating this, remember also to update the cache key with the same value in .github/workflows/gradle.yml
     javaVersion("11")
     mainClass("com.github.bjornvester.gww.AppKt")
     outputName("gw")
-    //windowsVsEdition("BuildTools")
+    //windowsVsEdition("BuildTools") // Needed for some installations for Windows. Not sure why.
     option("--no-fallback")
 }
 
@@ -28,14 +28,19 @@ tasks.withType<Wrapper> {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
-    /*
-    doFirst {
-        if(JavaVersion.current() != JavaVersion.VERSION_1_8){
-            throw GradleException("This build must be run with java 11")
-        }
+    // The source and target compatibility only functions as a hint to IntelliJ when importing project for selecting the correct JDK
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
-     */
+
+    doFirst {
+        // Use the GraalVM distribution for compiling the Kotlin classes
+        val extractGraalTask = tasks.getByName("extractGraalTooling", com.palantir.gradle.graal.ExtractGraalTask::class)
+        kotlinOptions.jdkHome = extractGraalTask.outputs.files.singleFile.absolutePath
+    }
 }
 
 val run by tasks.registering(JavaExec::class) {
